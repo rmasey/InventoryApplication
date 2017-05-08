26ab0c84cb81e4993d207397666214608a5b7592
rmasey <rmasey@hotmail.com>
2017-05-08 09:44:03 +0100

    Add files via upload

--- Author.java
+++ Author.java
@@ -1,7 +1,4 @@
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.PreparedStatement;
-import java.util.List;
+
 
 /* Each table you wish to access in your database requires a model class, like this example: */
 public class Author
@@ -26,64 +23,4 @@ public class Author
         return "Author ID: "  + AuthorID +  " First Name: "  + FirstName+ " Second Name: " + SecondName;
     }
 
-    /* Different models will require different read and write methods. Here is an example 'loadAll' method 
-     * which is passed the target list object to populate. */
-    public static void readAll(List<Author> list)
-    {
-        list.clear();       // Clear the target list first.
-
-        /* Create a new prepared statement object with the desired SQL query. */
-        PreparedStatement statement = Application.database.newStatement("SELECT AuthorID, FirstName, SecondName FROM Authors ORDER BY AuthorID"); 
-
-        if (statement != null)      // Assuming the statement correctly initated...
-        {
-            ResultSet results = Application.database.runQuery(statement);       // ...run the query!
-
-            if (results != null)        // If some results are returned from the query...
-            {
-                try {                               // ...add each one to the list.
-                    while (results.next()) {                                               
-                        list.add( new Author(results.getInt("AuthorID"), results.getString("FirstName"),results.getString("SecondName")));
-                    }
-                }
-                catch (SQLException resultsexception)       // Catch any error processing the results.
-                {
-                    System.out.println("Database result processing error: " + resultsexception.getMessage());
-                }
-            }
-        }
-    }
-
-    public static void deleteById(int AuthorID)
-    {
-        try{
-
-            PreparedStatement statement = Application.database.newStatement("DELETE FROM Authors WHERE AuthorID = ?");             
-            statement.setInt(1, AuthorID);
-
-            if (statement != null){
-                Application.database.executeUpdate(statement);
-            }
-        } catch (SQLException resultsexception){
-            System.out.println("Database result processing error: " + resultsexception.getMessage());
-        }
-    }
-
-    public void save()
-    {
-        PreparedStatement statement;
-
-        try{
-            statement = Application.database.newStatement("INSERT INTO Authors (FirstName, SecondName) VALUES (?, ?)");             
-            statement.setString(1, FirstName);    
-            statement.setString(2, SecondName);    
-
-            if (statement != null) {
-                Application.database.executeUpdate(statement);
-            }
-        }
-        catch (SQLException resultsexception) {
-            System.out.println("Database result processing error: " + resultsexception.getMessage());
-        }
-    }
 }
